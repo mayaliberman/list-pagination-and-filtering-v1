1f2ea505c1ab5c21ad2d4cd93d8c50cc3681de48
mayaliberman <50631141+mayaliberman@users.noreply.github.com>
2019-06-09 10:54:16 +0300

    extras with bug

--- js/script.js
+++ js/script.js
@@ -3,40 +3,18 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//two global variables one for locating the class
+//  student-item, the second will be used to show the number of students per page
 const listItems = document.getElementsByClassName('student-item');
 const numOfItems = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+//this function will create the 10 students per page when activated with the listItems as the first parameter.
 function showPage(list, page) {
   let startIdx = page * numOfItems - numOfItems;
   let endIdx = page * numOfItems;
   for (let i = 0; i < list.length; i++) {
+    //if the list node list is greater than the
+    // start index number and less than the index ending number the correct list will be showed
     if (i >= startIdx && i < endIdx) {
       list[i].style.display = 'block';
     } else {
@@ -45,11 +23,7 @@ function showPage(list, page) {
   }
 }
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
+//this function will append the links per page adding a new created classes and adding them like pagination, ul, li, and a
 const appendPageLinks = list => {
   let pages = Math.ceil(list.length / numOfItems);
   const newDiv = document.createElement('div');
@@ -57,31 +31,73 @@ const appendPageLinks = list => {
   document.getElementsByClassName('page')[0].appendChild(newDiv);
   const ulPage = document.createElement('ul');
   document.getElementsByClassName('pagination')[0].appendChild(ulPage);
-  const paginationActive = document.getElementsByClassName('.pagination li a.active')
   for (let i = 0; i < pages; i++) {
-     ulPage
-     .appendChild(document.createElement('li'))
-     .appendChild(document.createElement('a')).textContent = i + 1;
-   }
-
-   showPage(list, 1)
-
-   let pageLinks = document.querySelectorAll('a');
-   for(let i = 0; i < pages; i++) {
-      pageLinks[i].addEventListener('click', (e) =>{
-         showPage(list, (i+1))
-         
-          e.target.style.backgroundColor = '#4ba6c3'
-         
-         
-         
-      })
-
-   }
-   
-   
+    ulPage
+      .appendChild(document.createElement('li'))
+      .appendChild(document.createElement('a')).textContent = i + 1;
+  }
+  //activate the first page with the first 10 names.
+  showPage(list, 1);
+  //the link assignment of 'a'
+  let pageLinks = document.querySelectorAll('a');
+  //assigning the class to active
+  pageLinks[0].className = 'active';
+  // looping on all the page links with the add event. Re-assign the class name to an empty string
+  for (let i = 0; i < pages; i++) {
+    pageLinks[i].addEventListener('click', e => {
+      //thanks to my friend @Juan L who helped me with this
+      //  https://treehouse-fsjs-102.slack.com/archives/CBPMRMA82/p1560006620027500?thread_ts=1559984748.024500&cid=CBPMRMA82
+      for (let j = 0; j < pageLinks.length; j++) {
+        pageLinks[j].className = '';
+      }
+      //activation all the pages within the loop.
+      showPage(list, i + 1);
+      //event target reassign to an active class.
+      e.target.className = 'active';
+    });
+  }
 };
-
+//activate the function with the global variable.
 appendPageLinks(listItems);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search';
+document.getElementsByClassName('page-header')[0].appendChild(searchDiv);
+const searchInput = document.createElement('input');
+searchInput.placeholder = 'Search for Student...';
+
+const submitButton = document.createElement('button');
+submitButton.textContent = 'search';
+
+document.getElementsByClassName('student-search')[0].appendChild(searchInput);
+document.getElementsByClassName('student-search')[0].appendChild(submitButton);
+
+const h3 = document.querySelectorAll('h3');
+
+function searchBar(searchInput, listItems) {
+  for (let i = 0; i < listItems.length; i++) {
+    if (
+      searchInput.value.length !== 0 &&
+      h3[i].textContent.toLowerCase() === searchInput.value.toLowerCase()
+    ) {
+      listItems[i].style.display = 'block';
+      console.log('full name', listItems[i]);
+    }  
+    
+     if (
+      searchInput.value.length !== 0 &&
+      h3[i].textContent.toLowerCase()[0] === searchInput.value.toLowerCase()[0]
+    ) {
+      listItems[i].style.display = 'block';
+      console.log('only first letter', listItems[i]);
+    } else {
+      listItems[i].style.display = 'none';
+    }
+  }
+  searchInput.value = '';
+}
+
+submitButton.addEventListener('click', event => {
+  event.preventDefault();
+  searchBar(searchInput, listItems);
+});
